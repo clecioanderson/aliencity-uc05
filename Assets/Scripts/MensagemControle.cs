0706e62d3a53358f4943569a17455796529c794b
Clecio Ferreira <clecioferreira@tjrn.jus.br>
2021-04-18 22:38:41 -0300

    Commit Inicial
    Commit Inicial

---
+++ Assets/Scripts/MensagemControle.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MensagemControle : MonoBehaviour {
+
+	// Use this for initialization
+	public Text gameOverTxt;
+	public Text restartTxt;
+	public Text venceuTxt;
+
+	public bool gameOver;
+	public bool restart;
+	public bool venceu;
+
+	void Start () {
+		gameOver = false;
+		restart = false;
+		venceu = false;
+		gameOverTxt.text = "";
+		restartTxt.text = "";
+		venceuTxt.text = "";
+	}
+	
+	void Update () {
+		if (gameOver) {
+			restartTxt.text = "Pressione 'R' para Restart";
+			restart = true;
+		}
+		if (restart) {
+			if(Input.GetKeyDown(KeyCode.R))
+			{
+				Application.LoadLevel (Application.loadedLevel);
+			}
+		}
+	}
+
+	public void GameOver()
+	{
+		gameOverTxt.text = "Game Over!!!!";
+		gameOver = true;
+	}
+
+	public void Venceu()
+	{
+		venceuTxt.text = "Voce Venceu!!!";
+		restartTxt.text = "Pressione 'R' para Restart";
+		restart = true;
+	}
+}
